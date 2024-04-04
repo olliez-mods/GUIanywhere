509e64012de0b1ed565ed8ec76396a517f5a2374
olliez-mods <socksinthewash@gmail.com>
2024-04-04 14:06:17 -0400

    added featurs

--- capture_window.py
+++ capture_window.py
@@ -2,48 +2,57 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import mss
 
-class capt_window:
+class capture_window:
     def __init__(self, master, capt):
         self.master = master
         self.window = tk.Toplevel(master)
-        self.window.geometry("+50+50")
-        self.window.overrideredirect(True)
+        self.window.attributes('-alpha', 0.6)
         self.window.attributes("-topmost", True)
-
-        self.label = tk.Label(self.window)
-        self.label.pack()
-
-        self.window.bind("<<reload>>", self.reload)
-        self.window.bind("<<close>>", self.close)
-        self.window.bind("<<edit>>", self.edit_mode)
-        self.window.bind("<<display>>", self.display_mode)
-        self.window.bind("<B1-Motion>", self.drag_window)
 
         self.capt_pos = capt["capt_pos"]
         self.capt_size = capt["capt_size"]
 
-        self.run()
+        self.window.geometry(f"{self.capt_size[0]}x{self.capt_size[1]}+{self.capt_pos[0]}+{self.capt_pos[1]}")
+        self.window.overrideredirect(True)
 
-    def run(self):
-        with mss.mss() as sct:
-            monitor = {"left": self.capt_pos[0], "top": self.capt_pos[1], "width": self.capt_size[0], "height": self.capt_size[1]}
-            raw = sct.grab(monitor)
-        screenshot = ImageTk.PhotoImage(Image.frombytes('RGB', raw.size, raw.bgra, 'raw', 'BGRX'))
-        self.label.configure(image=screenshot)
-        self.label.image = screenshot
-        self.window.after(50, self.run)
+        self.inc_x = tk.Button(self.window, text="+ Width", command=lambda: self.adjust_size(6, 0))
+        self.inc_x.pack(side=tk.TOP, anchor='nw')
+        self.dec_x = tk.Button(self.window, text="- Width", command=lambda: self.adjust_size(-6, 0))
+        self.dec_x.pack(side=tk.TOP, anchor='nw')
+        self.inc_y = tk.Button(self.window, text="+ Height", command=lambda: self.adjust_size(0, 6))
+        self.inc_y.pack(side=tk.TOP, anchor='nw')
+        self.dec_y = tk.Button(self.window, text="- Height", command=lambda: self.adjust_size(0, -6))
+        self.dec_y.pack(side=tk.TOP, anchor='nw')
 
+        self.window.bind("<<close>>", self.close)
+        
+        self.window.bind("<B1-Motion>", self.drag_window)
+        self.window.bind("<Button-1>", self.window_clicked)
+        self.window.bind("<ButtonRelease-1>", self.window_unclicked)
+
+    def adjust_size(self, x, y):
+        self.capt_size[0] += x
+        self.capt_size[1] += y
+        self.window.geometry(f"{self.capt_size[0]}x{self.capt_size[1]}")
+        
+
+    def window_clicked(self, event):
+        self.dragging = True
+        self.mouse_x = event.x
+        self.mouse_y = event.y
+    def window_unclicked(self, event):
+        self.dragging = False
     def drag_window(self, event):
-        self.window.geometry(f"+{int(event.x_root - self.capt_size[0]/2)}+{int(event.y_root - self.capt_size[1]/2)}")
+        if self.dragging:
+            deltax = event.x - self.mouse_x
+            deltay = event.y - self.mouse_y
+            x = self.window.winfo_x() + deltax
+            y = self.window.winfo_y() + deltay
+            self.window.geometry(f"+{x}+{y}")
+            self.capt_pos[0], self.capt_pos[1] = x, y
 
-    def reload(self, event):
-        self.window.destroy()
-        self.__init__()
     def close(self, event):
         self.window.destroy()
-    def edit_mode(self, event):
-        self.window.overrideredirect(False)
-    def display_mode(self, event):
-        self.window.overrideredirect(True)
+
     def send_event(self, evstr):
         self.window.event_generate(evstr)
--- manger.py
+++ manger.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-from capture_window import capt_window
-from outline_window import outline_window
+from display_window import display_window
+from capture_window import capture_window
 
 root = tk.Tk()
 root.title("GuiAnywhere")
@@ -11,24 +11,22 @@ def send_event_all(evstr):
     for window in capt_windows:
         window.send_event(evstr)
 
-def bigger_x():
-    capt_windows[0].capt_size[0]+=1
-def bigger_y():
-    capt_windows[0].capt_size[1]+=1
-edit_button = tk.Button(root, text="bigger_x", command=lambda: bigger_x())
+
+edit_button = tk.Button(root, text="Start Edit Mode", command=lambda: send_event_all("<<edit>>"))
 edit_button.pack()
-display_button = tk.Button(root, text="bigger_y", command=lambda: bigger_y())
+display_button = tk.Button(root, text="End Edit Mode", command=lambda: send_event_all("<<display>>"))
 display_button.pack()
 
 
+
 capts = []
-capts.append({"capt_pos":[-500, 30], "capt_size":[300, 300]})
+capts.append({"capt_pos":[500, 30], "capt_size":[300, 300]})
 capts.append({"capt_pos":[100, 100], "capt_size":[200, 200]})
 
 lebel = tk.Label(root, text="HELLO")
 lebel.pack()
 
-o = outline_window(root, capts[0])
-capt_windows.append(capt_window(root, capts[0]))
-#capt_windows.append(cpt_window(root, capts[1]))
+#o = outline_window(root, capts[0])
+capt_windows.append(display_window(root, capts[0]))
+capt_windows.append(display_window(root, capts[1]))
 root.mainloop()
